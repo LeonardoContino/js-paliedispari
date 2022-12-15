266ff2cf3f5b5b7e6aca42152a8ba8ce38ceaeee
LeonardoContino <leoconti2000@hotmail.it>
2022-12-15 17:31:58 +0100

    generato un numero random da 1 a 5

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -2,10 +2,18 @@
 
 /*
 Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5 tramiite un form.
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5
+ tramiite un form.
 Generiamo un numero random (sempre da 1 a 5) per il computer
  (usando una funzione).
 Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Stabiliamo se la somma dei due numeri è pari o dispari
+ (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+const user = document.getElementById("usernumber");
+
+const max = 5;
+const min = 1;
+const cpu = Math.floor(Math.random() * max + min);
+console.log(cpu);
