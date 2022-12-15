6bb2866f6f31df8fc55eef15fce7a6ba9998bf08
LeonardoContino <leoconti2000@hotmail.it>
2022-12-15 15:21:48 +0100

    first commit

---
+++ palidroma/main.js
@@ -0,0 +1,18 @@
+// console.log("js ok");
+
+/*
+Palidroma
+Chiedere all’utente di inserire una parola tramiite un form
+Creare una funzione per capire se la parola inserita è palindroma
+
+*/
+
+// es palidroma
+/**
+ * 1. aggiungere input e bottom
+ * 2.aggiungere elemento in dom
+ * 3.addevent con buttom e stampare in console il nome del input come array
+ * 4. creare una funzione con while se
+ * la parola girata al contrario non cambia allora è un palidroma
+ * sennò è false
+ *  */
